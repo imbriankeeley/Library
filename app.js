f149f4086cc42ed82c847a1a82a6adca3e1971ea
imbriankeeley <bk3700@proton.me>
2023-12-14 23:37:56 -0500

    Added logic for app and styled cards

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 
-const button = document.querySelector('.btn')
-
-
 
 let myLibrary = [];
 
@@ -12,12 +9,59 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-
-
-function addBookToLibrary() {
+function render() {
+    let libraryEl = document.querySelector('.library');
+    libraryEl.innerHTML = '';
+    for (let i = 0; i < myLibrary.length; i++){
+        let book = myLibrary[i];
+        let bookEl = document.createElement('div');
+        bookEl.innerHTML = 
+        `
+        <div class="card">
+            <h1>${book.title}</h1>
+            <h2>by ${book.author}</h2>
+            <h2>${book.pages} pages</h2>
+            <button class="read-btn" onclick="toggleRead(${i})">${book.read ? 'Read' : 'Not Read Yet'}</button>
+            <button class="remove-btn" onclick="removeBook(${i})">Remove</button>
+        </div>
+        `
+        libraryEl.appendChild(bookEl);
+    }
     
 }
 
-myLibrary.forEach(element => {
-    
-});
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
+function toggleRead(index) {
+    myLibrary[index].toggleRead();
+    render();
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    render()
+}
+
+function addBookToLibrary() {
+    let title = document.querySelector('#title').value
+    let author = document.querySelector('#author').value
+    let pages = document.querySelector('#pages').value
+    let read = document.querySelector('#read').checked
+
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    render();
+}
+
+let newBookbtn = document.querySelector("#add-book")
+newBookbtn.addEventListener('click', function() {
+    let newBookForm = document.querySelector('#add-book-form');
+    newBookForm.style.display = 'block';
+})
+
+document.querySelector('#add-book-form').addEventListener("submit", function() {
+    event.preventDefault();
+    addBookToLibrary()
+})
