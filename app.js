02f980cf38de227c5daad089cfc0887fa8ea0f3b
imbriankeeley <bk3700@proton.me>
2023-12-14 20:44:21 -0500

    Added boilerplate and jump started logic

---
+++ app.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.addBookToLibrary();
+}
+
+function addBookToLibrary() {
+    myLibrary.push
+}
+
+myLibrary.forEach(element => {
+    
+});
+
+const harryPotter = new Book('Harry Potter', 'Jk Rowling', '700 pages', 'have not read')
